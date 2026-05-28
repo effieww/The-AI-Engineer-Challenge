c69a79a82868601406f3fab588036bc132e49fb6
Effie Wang <effie.ww@gmail.com>
2026-05-28 16:59:45 -0700

    Remove dotenv import for Vercel

--- api/index.py
+++ api/index.py
@@ -3,9 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from openai import OpenAI
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = FastAPI()
 
